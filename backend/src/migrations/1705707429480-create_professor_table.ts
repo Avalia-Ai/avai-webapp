fade1206882b4a536b7bdce9498c86f851f59661
GMichaloski <gustavomichaloski@gmail.com>
2024-01-19 20:42:32 -0300

    :sparkles: add Professor table

---
+++ backend/src/migrations/1705707429480-create_professor_table.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateProfessorTable1705707429480 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "Professor" (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP NOT NULL
+                )`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("Professor");
+  }
+}
