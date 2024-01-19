01d8f0378050f31df0d24f166f3c946d4f6d8d59
GMichaloski <gustavomichaloski@gmail.com>
2024-01-19 16:18:48 -0300

    :sparkles: create index.ts

---
+++ backend/index.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
